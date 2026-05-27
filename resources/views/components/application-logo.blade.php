ac0b3ed4196442c2428c92fa5a21fa59c4ad2c77
Willem van Veelen <social@garagebook.nl>
2026-05-27 13:15:26 +0200

    Fix logo component attributes

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="/images/logo_25_meetthespeed_edited_edited.avif" alt="Speedweek" { $attributes->merge(['class' => 'h-10 w-auto object-contain']) }>
+<img src="/images/logo_25_meetthespeed_edited_edited.avif" alt="Speedweek" {{ $attributes->merge(['class' => 'h-10 w-auto object-contain']) }}>
