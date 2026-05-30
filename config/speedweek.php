9e0767883c128939e3ba70248a6781abefb44f8b
Willem van Veelen <social@garagebook.nl>
2026-05-31 00:25:31 +0200

    Update Speedweek contact details

--- config/speedweek.php
+++ config/speedweek.php
@@ -2,8 +2,8 @@
 
 return [
     'contact' => [
-        'name' => env('SPEEDWEEK_CONTACT_NAME', 'Speedweek operations'),
-        'email' => env('SPEEDWEEK_CONTACT_EMAIL', 'info@speedweek.local'),
-        'phone' => env('SPEEDWEEK_CONTACT_PHONE', '+31 000 000 000'),
+        'name' => env('SPEEDWEEK_CONTACT_NAME', 'Gino Zeelenberg'),
+        'email' => env('SPEEDWEEK_CONTACT_EMAIL', 'info@zeelenbergracing.com'),
+        'phone' => env('SPEEDWEEK_CONTACT_PHONE', '+31 6 21275825'),
     ],
 ];
